bd889685bdf4f14a4b6d2dd174ba1f060a6d31b4
CarlosRodZX <150416580+CarlosRodZX@users.noreply.github.com>
2024-08-22 02:46:54 +0000

    Added JavaScript code to HTML and space

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("Start")
+console.log("Hello")
+
+const name = "Jose"
+console.log(name)
+
+const age = 25
+console.log(age)
+
+let food = "Pizza"
+console.log(food)
+
+let favoriteAnimal = "Shark"
+console.log(favoriteAnimal)
+
+
+let myName = "Jose"
+let instructorName = "Ash"
+let studentName = "Veronica"
+console.log("myName start value is Jeni")
+
+myName = "Jose"
+
+console.log(" myName Value is", myName)
+// console.log("The instructor's name is ", instructorName)
+// console.log("My student's name is ", studentName)
